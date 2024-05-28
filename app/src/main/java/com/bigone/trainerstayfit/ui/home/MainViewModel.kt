911deace059669fceaa97e0b455f59f9890d8727
Shamshad <muhammedshamshadp@gmail.com>
2024-05-29 00:39:11 +0530

    Home page fixes

--- app/src/main/java/com/bigone/trainerstayfit/ui/home/MainViewModel.kt
+++ app/src/main/java/com/bigone/trainerstayfit/ui/home/MainViewModel.kt
@@ -2,13 +2,49 @@ package com.bigOne.trainerstayfit.ui.home
 
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.*
+import com.bigOne.trainerstayfit.datas.model.UserData
 import com.bigOne.trainerstayfit.datas.repository.FirestoreRepository
 
 class  MainViewModel(application: Application) : AndroidViewModel(application) {
     private val firebaseRepository: FirestoreRepository = FirestoreRepository(getApplication())
 
+    fun getUserData(): LiveData<UserData?> {
+        val _savedUserData = MutableLiveData<UserData?> ()
+        firebaseRepository.getUserData()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null) {
+                    val item = document.data
+                    Log.e("userData", item.toString())
+                    if(item!=null) {
+                        val userData = mapToUserData(item)
+                        _savedUserData.value = userData
+                    }else{
+                        _savedUserData.value =null
+                    }
+                }
+            }else{
+                _savedUserData.value =null
+            }
+        }
+        return _savedUserData
+    }
+    private fun mapToUserData(data: Map<String, Any>): UserData {
+        return UserData(
+            sex = data["sex"] as String,
+            dob = data["dob"] as String,
+            weight = data["weight"] as String,
+            height = data["height"] as String,
+            name = data["name"] as String,
+            email = data["email"] as String,
+            id = data["id"] as String,
+            img = data["img"] as String,
+            isTrainer = data["trainer"] as Boolean
 
+        )
+    }
 
 
 
